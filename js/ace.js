8be0e91a4e4a9a17dad43173e6f3e95c5de07e33
Joe Vaughn <31394713+olethrosjv@users.noreply.github.com>
2019-05-21 12:32:31 -0700

    Add files via upload

--- js/ace.js
+++ js/ace.js
@@ -45,7 +45,7 @@ function apiCall() {
 
 			var iconLink = "https://" + response.current.condition.icon;
 
-			$("#city").html("<h3>" + fullLocation + "</h3>");
+			$("#city").html("<h4>" + fullLocation + "</h4>");
 			$("#description").html(description + " | " + "<a href='#' id='change'>" + temperature + "&#176;" + unit + "</a>");
 			$("#icon").html('<img src="' + iconLink + '">');
 		}
@@ -61,3 +61,38 @@ function error() {
 	$("#city").html("There has been an error.</br>Try again later!");
 $("#description").html("");
 }
+
+
+// new slideshow
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
+
+
